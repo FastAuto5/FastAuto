92becb916ef2b86a78c60963a6a8eb1927a71945
ahmadaburezeq1998 <ahmademadaburezeq1998@gmail.com>
2021-08-03 05:03:41 +0300

    created the local storage and continued the form :

--- js/story.js
+++ js/story.js
@@ -1,35 +1,73 @@
 "use strict"
-let prevreviewselement=document.getElementById('prevreviews');
-
-let nameelement=document.getElementById('nameid');
-let sexelement=document.getElementById('sexid');
-let ownelement=document.getElementById('ownid');
-let storyelement=document.getElementById('storyid');
-let imgelement=document.getElementById('imgid');
-let rateelement=document.getElementById('rateid');
+let inputstoryyelement = document.getElementById('inputstory');
+let prevreviewselement = document.getElementById('prevreviews');
 
 
-function Review(name,sex,own,story,img,rate) {
-    this.name=name;
-    this.sex=sex;
-    this.own=own;
-    this.story=story;
-    this.img=img;
-    this.rate=rate;
+function Review(name, sex, own, story, img, rate) {
+    this.name = name;
+    this.sex = sex;
+    this.own = own;
+    this.story = story;
+    this.img = img;
+    this.rate = rate;
 
-    this.prototype.allreviews.push(this);
-    // updateStorage();
+    Review.prototype.allreviews.push(this);
+   updateStorage();
 }
-Review.prototype.allreviews=[];
+Review.prototype.allreviews = [];
 
 
-function renderstories() {
-    for (let index = 0; index < Review.prototype.allreviews.length; index++) {
-        let renderpar=document.createElement('p');
-        prevreviewselement.appendChild('renderpar');
-        renderpar.textContent=`
-        name: ${Review.prototype.allreviews[index].name}`
-        
-    }
+Review.prototype.renderstories = function () {
+
+    let renderpar = document.createElement('p');
+
+    
+    prevreviewselement.appendChild(renderpar);
+
+    renderpar.textContent =
+        `Name : ${this.name}  
+        Sex : ${this.sex} Ownership: ${this.own} 
+        My thoughts about this Car : ${this.story} 
+        how would oyu rate your experience with this car : html ${this.rate} `;
+
+    console.log(renderpar);
+    updateStorage();
 }
-renderstories();
+
+inputstoryyelement.addEventListener('submit', submitter);
+
+
+
+function updateStorage() {
+     let stringArr=JSON.stringify(Review.prototype.allreviews);
+    //  console.log(Review.prototype.allreviews);
+    //  console.log(stringArr);
+    localStorage.setItem('allreviewskey',Review.prototype.allreviews);
+
+    
+if (Review.prototype.allreviews.lemgth!=null) {
+    Review.prototype.allreviews=parsedArr
+} 
+renderpar();
+}
+
+
+
+
+function submitter(e) {
+    e.preventDefault();
+
+    let nameelement = e.target.nameinput.value;
+    let sexelement = e.target.sexname.value;
+    let ownelement = e.target.ownname.value;
+    let storyelement = e.target.storyname.value;;
+    let imgelement = e.target.imgname.value;
+    let rateelement = e.target.ratename.value;
+
+    let newreview = new Review(nameelement, sexelement, ownelement, storyelement, imgelement, rateelement);
+    console.log(newreview);
+
+    newreview.renderstories();
+
+
+}
